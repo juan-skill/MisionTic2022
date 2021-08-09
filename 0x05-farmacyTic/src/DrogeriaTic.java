5390acb568155ad0ab80b066f3f5c3a1deb0d3cb
juan-skill <1294@holbertonschool.com>
2021-08-09 14:50:20 +0000

    [fix] fix control flow to receive the input string from System.in 	modified:   src/DrogeriaTic.java

--- 0x05-farmacyTic/src/DrogeriaTic.java
+++ 0x05-farmacyTic/src/DrogeriaTic.java
@@ -18,38 +18,52 @@ public class DrogeriaTic {
 
             if (input.charAt(i) == '&' || i == input.length() - 1)
             {
-                System.out.println("simbol &");
                 parse.add(word);
                 word = "";
             }
             
         }
 
-        Iterator<String> it = parse.iterator();
-        while(it.hasNext()) {
-            System.out.println(it.next());
-        }
-
-        return null;
+        return parse;
     }
 
     public static void main(String[] args) throws Exception
     {
         Scanner sc = new Scanner(System.in);
+        ArrayList<String> parse = null;
+        Producto product = null;
+        Iterator<Producto> it = null;
+        ArrayList<Producto> listProducts = new ArrayList<Producto>();
         int op = 0;
 
         do {
-            procesarEntrada("1&Alimentos&nombre&codigo&precio&marca&presentacion");
-            op = Integer.parseInt(sc.nextLine());
+            //parse = procesarEntrada("1&Alimentos&nombre&codigo&precio&marca&presentacion");
+            parse = procesarEntrada(sc.nextLine());
+            op = Integer.parseInt(parse.get(0));
             switch (op)
             {
-                case 1: //
-                        //String input = sc.nextLine();
+                case 1: // add a new product
                         System.out.println("case 1");
-
+                        
+                        if ("Medicamento".compareToIgnoreCase(parse.get(1)) >= 1)
+                        {
+                            product = new Medicamento(parse.get(6), parse.get(2), Long.parseLong(parse.get(4)), Float.parseFloat(parse.get(4)), parse.get(5));
+                        }
+                        else if ("Alimentos".compareToIgnoreCase(parse.get(1)) >= 1)
+                        {
+                            product = new Alimentos(parse.get(6), parse.get(2), Long.parseLong(parse.get(4)), Float.parseFloat(parse.get(4)), parse.get(5));
+                        }
+                        listProducts.add(product);
                         break;
-                case 2:
+
+                case 2: // show all of the products
                         System.out.println("case 2");
+
+                        it = listProducts.iterator();
+                        System.out.println("***DrogueriaTic***");
+                        while(it.hasNext()) {
+                            System.out.println(it.next());
+                        }
                         break;                
             }
 
