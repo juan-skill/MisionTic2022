221a284de81398a369b061dc94ce0d4abfde1c34
juan-skill <1294@holbertonschool.com>
2021-08-20 15:21:16 +0000

    [add] Implementing the prototypes functions 	new file:   src/FuncionesProducto.java

---
+++ 0x06-helatic_system/src/FuncionesProducto.java
@@ -0,0 +1,59 @@
+import java.util.ArrayList;
+
+public class FuncionesProducto
+{
+    /**
+     * FuncionesProducto - constructor function
+     */
+    public FuncionesProducto()
+    {}
+
+    /**
+     * obtenerProductos - generates a list of names without duplicates
+     * 
+     * @param list
+     * @return
+     */
+    public ArrayList<String> obtenerProductos(ArrayList<String> list)
+    {
+        return null;
+    }
+
+    /**
+     * obtenerPosicionesProductosFaltantes - generates a list with the products' indexes missing
+     * 
+     * @param IPos
+     * @param tipos
+     * @param cat
+     * @return
+     */
+    public ArrayList<Integer> obtenerPosicionesProductosFaltantes(ArrayList<Integer> IPos, ArrayList<String> tipos, String cat)
+    {
+        return null;
+    }
+
+    /**
+     * obtenerFaltantes - returns a list with the products that HelaTic is interested
+     * 
+     * @param lOtro
+     * @param lHelaTic
+     * @return
+     */
+    public ArrayList<String> obtenerFaltantes(ArrayList<String> lOtro, ArrayList<String> lHelaTic)
+    {
+        return null;
+    }
+
+    /**
+     * obtenerProductosIntercambiables - finds the missing products entered in both list, return an exchange number each other
+     * 
+     * @param lOtro
+     * @param lHelaTic
+     * @return
+     */
+    public Integer obtenerProductosIntercambiables(ArrayList<String> lOtro, ArrayList<String> lHelaTic)
+    {
+        return 0;
+    }
+
+}
